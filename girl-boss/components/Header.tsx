1dca27d0b73e91e21ac3ddaa7461f337a9da5357
Sophia Phu <sophiatiffphu@gmail.com>
2025-11-09 20:54:53 -0600

    moved navigation somewhere else yay

--- girl-boss/components/Header.tsx
+++ girl-boss/components/Header.tsx
@@ -4,7 +4,7 @@ import { Menu } from "lucide-react";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
-import NavigationMenu from "@/app/components/Navigation";
+import NavigationMenu from "@/components/Navigation";
 
 export default function Header() {
   const router = useRouter();
